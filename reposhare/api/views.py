be9958e632a9dfb79ed8c1849fa11d31509820c8
piter <piter@piterdev.me>
2024-12-31 17:41:40 +0100

    Chore: remove rest framework

--- reposhare/api/views.py
+++ reposhare/api/views.py
@@ -1,24 +1,8 @@
 from django.shortcuts import render
-from rest_framework import generics, status
-from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
-from rest_framework.views import APIView
 from .utils.github_api import GitHubAPI
 from django.http import JsonResponse
-
-class PostListCreate(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-    def delete(self, request, *args, **kwargs):
-        Post.objects.all().delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-class PostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer 
-    lookup_field = "pk"
 
 def fetch_repo_info(request):
     token = "nuh uh"  # Ideally, load from environment variables
